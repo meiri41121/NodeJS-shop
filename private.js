1fc848793d0cd4594817551bdc4c7a1675d8200a
Meiri Fenesh <97239420+meiri41121@users.noreply.github.com>
2022-05-10 21:47:53 +0300

    Delete private.js

--- private.js
+++
@@ -1,3 +0,0 @@
-exports.MONGODB_URI = 'mongodb+srv://meiri:12342332@cluster0.k8zp4.mongodb.net/SHOPNODEJS?retryWrites=true&w=majority';
-exports.api_key = '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';
-    
